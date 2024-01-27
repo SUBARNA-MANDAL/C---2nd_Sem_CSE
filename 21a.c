ceee5b5e16b7d922526564bcb7fb37bb0d936747
Subarna Mandal <subarnamandal17@gmail.com>
2024-01-28 00:11:38 +0530

    updated 21a

--- 21a.c
+++ 21a.c
@@ -3,7 +3,7 @@ Write a program to calculate
         y = ∑(x = 1 to n) (1 / fact(x))
         Where fact()) is user defined function to calculate factorial. 
 
-*/
+
 #include<stdio.h>
 int fact(int n){
     int i,r=1;
@@ -21,4 +21,23 @@ void main(){
     }
 
     printf("\nSo, the result is = %f",r);
+}*/
+
+
+#include<stdio.h>
+int fact(int n){
+    int i,r=1;
+    for(i=1;i<n+1;i++)  r*=i;
+    return r;
+}
+void main(){
+    int x,i;
+    float r;
+    printf("Enter the x value --> ");
+    scanf("%d",&x);
+
+    for(i=1;i<x+1;i++){
+        r += (float)1/(fact(i));
+    }
+    printf("\nSo, the result is = %lf",r);
 }
