d163204b3c67a47a6533c6fb3b58cf73e4ea1b77
Subarna Mandal <subarnamandal17@gmail.com>
2024-01-31 00:11:59 +0530

    updated 25a to 26a

--- 25a.c
+++ 25a.c
@@ -8,15 +8,39 @@ allocation for better memory utilization.
 */
 
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 struct student{
     char name[10];
     int roll;
-    float marks;
+    int marks;
 };
+void topscorer(int n,struct student* students){
+    int max=0,i;
+    for(i=0;i<n;i++){
+        if(students[i].marks>max)   max=students[i].marks;
+    }
+    printf("\nHighest marks is %d",max);
+    for(i=0;i<n;i++){
+        if(max==students[i].marks)  printf("\n%s got the highest marks.");
+
+    }
+}
 void main(){
-    int n;
+    int n,i;
     printf("Enter the number of students --> ");
     scanf("%d",&n);
+    struct student *students = (struct student *)malloc(n*sizeof(n));
 
-    
+    printf("<-----------Enter %d students data ------------->",n);
+    for(i=0;i<n;i++){
+        printf("\nEnter %d students name --> ",i+1);
+        scanf("%s", students[i].name);
+        printf("Enter %d students roll --> ",i+1);
+        scanf("%d",&students[i].roll);
+        printf("Enter %d students marks --> ",i+1);
+        scanf("%d",&students[i].marks);
+    }
+    topscorer(n,students);
+    free(students);
 }
---
+++ 26a.c
@@ -0,0 +1,13 @@
+/*
+
+
+Write a program to create a filename "Profile.txt" and write
+your details in that file and copy the content of that file 
+into another file "myfile.txt"
+
+
+*/
+#include<stdio.h>
+void main(){
+    FILE *ptr = fopen("profile.txt","w");
+}
